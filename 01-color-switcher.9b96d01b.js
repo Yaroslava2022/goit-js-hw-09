82351dfe63d5fb237e10f59ad96ebe875ac6022f
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-03-20 01:45:42 +0000

    Deploying to gh-pages from @ Yaroslava2022/goit-js-hw-09@3baac2a97943f681434a88e6f255348ca64c94b6 🚀

---
+++ 01-color-switcher.9b96d01b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9b96d01b.js.map
---
+++ 01-color-switcher.eff0c13a.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(()=>{n=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.eff0c13a.js.map
